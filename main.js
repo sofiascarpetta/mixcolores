510eab1b451a814467707df5d15c4cd56761bc2b
ssofiascarpetta <ssofiascarpetta@gmail.com>
2023-05-18 21:05:54 -0300

    segundaPreEntrega

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let nombre = prompt("Por favor, ingresa tu nombre");
+let mensaje = "Bienvenidx, " + nombre + " disfruta de tus nuevos colores";
+alert(mensaje);
+
 function mezclar() {
     // parseInt(Rojo1, 16);
 
@@ -14,12 +18,10 @@ function mezclar() {
     let azulNuevo = fusionarColores (colorUnoDesintegrado.b, colorDosDesintegrado.b);
 
     let colorNuevo = `rgb(${rojoNuevo}, ${verdeNuevo}, ${azulNuevo})`;
-    
 
     document.querySelector("#resultado").style.backgroundColor = colorNuevo
-
+    alert(colorNuevo)
 }
-
 function extraerColor(colores) {
     let rojo = '';
     let verde = '';
@@ -46,6 +48,44 @@ function fusionarColores(c1, c2) {
     let c_promedio = (c1 + c2) / 2;
     return c_promedio;
 }
+
+let arrayColores = [
+  {id:1, tipo: "otoño", color: "marron", rgb:"rgb(92, 59, 45)"},
+  {id:2, tipo: "otoño", color: "naranja", rgb:"rgb(255, 127, 0)"},
+  {id:3, tipo: "invierno", color: "azul", rgb:"rgb(5, 42, 102)"},
+  {id:4, tipo: "invierno", color: "celeste", rgb:"rgb(130, 179, 232)"},
+  {id:5, tipo: "verano", color: "rojo", rgb:"rgb(178, 46, 46)"},
+  {id:6, tipo: "verano", color: "amarillo", rgb:"rgb(248, 214, 42)"},
+  {id:7, tipo: "primavera", color: "rosado", rgb:"rgb(254, 42, 144)"},
+  {id:8, tipo: "primavera", color: "verde", rgb:"rgb(0, 158, 84)"},
+ ]
+function buscarTiposDeColores(tipo){
+  let coloresEncontrados = arrayColores.filter(color => color.tipo === tipo);
+  console.log(coloresEncontrados);
+  agregarColores(coloresEncontrados)
+}
+function agregarColores(colores){
+  const divParent = document.querySelector("#coloresDeEstaciones");
+  divParent.innerHTML = `
+  <div style="background-color:${colores[0].rgb}" class="cuadrado"> 
+  </div>
+  <div style="background-color:${colores[1].rgb}" class="cuadrado"> 
+  </div>
+  `;
+}
+
+//function filterItems(query) {
+//return fruits.filter(function(el)
+//words.filter(word => word.length > 6)
+//let buscarTiposDeColores = ['otoño', 'invierno', 'verano', 'primavera'];
+//let resultadoDeBusqueda = buscarTiposDeColores.filter(buscarTiposDeColores => buscarTiposDeColores ===);
+//console.log(resultadoDeBusqueda)
+ 
+
+
+
+//for (let item of buscarTiposDeColores){
+//console.log(item.BuscarTiposDeColores)
 
 
 
